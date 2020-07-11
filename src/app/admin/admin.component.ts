319e6aa7756d920e049f74587a33a8dcdb157c5f
Jyotirmoy Pal <jyotirmoypal077@gmail.com>
2020-07-11 14:05:56 +0530

    Starting to work for get response obj 11

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Router, ActivatedRoute, Resolve } from '@angular/router';
 import { ConfigService } from '../config.service'
 import {
   HttpClient,
   HttpHeaders,
   HttpErrorResponse,
 } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -23,7 +24,6 @@ export class AdminComponent implements OnInit {
   lastName;
   photoUrl;
 
-
   public accessToken = '';
 
   /*httpOptions = {
@@ -36,7 +36,8 @@ export class AdminComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private service: ConfigService,
-    private http: HttpClient
+    private http: HttpClient,
+    //private resolve: Resolve<any>
   ) {}
 
   ngOnInit() {
@@ -54,35 +55,37 @@ export class AdminComponent implements OnInit {
           console.log(this.accessToken + 'From component ');
           localStorage.setItem('token', this.accessToken);
 
-              console.log('hi I will be editing here ---------');
-              console.log(this.accessToken + 'hello------------');
+          console.log('hi I will be editing here ---------');
+          console.log(this.accessToken + 'hello------------');
 
-              //3rd Get
-              const headers = new HttpHeaders({
-                'Authorization': `Bearer ${this.accessToken}`,
-                'Connection': 'Keep-Alive',
-              });
-              this.http
-                .get<any>(
-                  'https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))',
-                  { headers }
-                )
-                .subscribe((data: any) => {
-                  console.log(JSON.stringify(data));
-                  this.json = data.total;
+          //3rd Get
+          const headers = new HttpHeaders({
+            Authorization: `Bearer ${this.accessToken}`,
+            Connection: 'Keep-Alive',
+          });
+          this.http
+            .get<any>(
+              'https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))',
+              { headers }
+            )
+            .subscribe((data: any) => {
+              console.log(JSON.stringify(data));
+              this.json = data;
 
-                  this.firstName = data.firstName.localized.en_US;
-                  this.lastName = data.lastName.localized.en_US;
-                  this.photoUrl =(data.profilePicture.displayImage`~`.elements[0].identifiers[0].identifier) 
-                  console.log(this.firstName);
-                  console.log(this.lastName);
-                  console.log(this.photoUrl);
+              this.firstName = data.firstName.localized.en_US;
+              this.lastName = data.lastName.localized.en_US;
+              const photoPic = JSON.parse(data);
+              this.photoUrl=(
+                photoPic['profilePicture']['displayImage~'].elements[0]
+                  .identifiers[0].identifier
+              );
+              //this.photoUrl =data.profilePicture.displayImage~.elements[0].identifiers[0].identifier
+              console.log(this.firstName);
+              console.log(this.lastName);
+              console.log(this.photoUrl);
 
-                  console.log(this.json);
-                });
-
-
-
+              console.log(this.json);
+            });
         },
         (err: HttpErrorResponse) => {
           if (err.error instanceof Error) {
@@ -92,7 +95,5 @@ export class AdminComponent implements OnInit {
           }
         }
       );
-
-    
   }
 }
